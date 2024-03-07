f4e4191930d8b862b9fcf24fc207f901db248c1a
gahyeon.sin <singahyeon@geeksloft.com>
2024-03-07 13:27:01 +0900

    openseadragon overlay 옵션 테스트

--- js-project/main.js
+++ js-project/main.js
@@ -10,29 +10,58 @@ const viewer = OpenSeadragon({
     // zoom 설정
     minZoomLevel: 0.5, // 최소 배율 0.5
     maxZoomLevel: 32, // 최대 배율 20
-    zoomPerScroll: 2, // 한 번 줌인 시 배율 x2
+    gestureSettingsMouse: {
+        clickToZoom: false // click할 때 zoom-in 되는 기능 false
+    },
 
     // viewer navigator 설정
     showNavigator: true,
-    navigatorPosition: "BOTTOM_RIGHT"
+    navigatorPosition: "BOTTOM_RIGHT",
+
+    // overlays: [{
+    //     id: 'fixed-el-0', // Green overlay
+    //     x: 0.059,
+    //     y: 0.08
+    // }]
 });
 
-viewer.addHandler('canvas-click', function(event) {
+let DOMArr = [];
+
+const handleAddComment = (event) => {
     // The canvas-click event gives us a position in web coordinates.
     var webPoint = event.position;
-
+        
     // Convert that to viewport coordinates, the lingua franca of OpenSeadragon coordinates.
     var viewportPoint = viewer.viewport.pointFromPixel(webPoint);
 
     // Convert from viewport coordinates to image coordinates.
     var imagePoint = viewer.viewport.viewportToImageCoordinates(viewportPoint);
 
-    // Show the results.
-    console.log(webPoint.toString(), viewportPoint.toString(), imagePoint.toString());
-});
+    console.log(viewportPoint, viewportPoint.toString(),imagePoint,  imagePoint.toString())
+
+    const fixedElDOM = document.createElement("div");
+    fixedElDOM.id = `fixed-el-${DOMArr.length}`;
+    fixedElDOM.className = 'fixed-el';
+    DOMArr.push(fixedElDOM.id);
+    console.log('DOMArr',DOMArr, fixedElDOM);
+    viewer.addOverlay({
+        element: fixedElDOM,
+        location: new OpenSeadragon.Point(viewportPoint.x, viewportPoint.y)
+    })
+}
+
+function activateComment() {
+    viewer.addHandler('canvas-click', handleAddComment);
+}
+
+function deactivateComment() {
+    viewer.removeHandler('canvas-click', handleAddComment);
+}
+
 
 const detectZoom = (zoom) => {
-    console.log('current zoom level: ', Math.round(zoom.zoom * 10) / 10)
+    console.log('current zoom level: ', Math.round(zoom.zoom * 10) / 10);
+
 }
   
 viewer.addHandler('zoom', detectZoom)
